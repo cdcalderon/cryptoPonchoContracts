16650d7656436274bc7f26748639ec29cbfb49b9
Carlos Calderon <cdcalderon@gmail.com>
2022-01-31 18:57:34 -0600

    set up contract

--- contracts/CryptoPoncho.sol
+++ contracts/CryptoPoncho.sol
@@ -1,13 +1,38 @@
 // SPDX-License-Identifier:MIT
-
 pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 /* Errors */
 
-/* Type declarations */
+contract CryptoPoncho is ERC721Enumerable, Ownable {
+    /* Type declarations */
+    using Strings for uint256;
 
-/* State variables */
+    /* State variables */
+    string baseURI;
+    string public baseExtension = ".json";
+    uint256 public cost;
+    uint256 public maxSupply;
+    uint256 public maxMintAmount = 1;
+    uint256 public timeDeployed;
+    uint256 public allowMintingAfter = 0;
+    bool public isPaused = false;
+    bool public isRevealed = true;
+    string public notRevealedUri;
 
-/* Events */
+    /* Events */
 
-/* View / Pure functions */
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+    function setBaseExtension(string memory _newBaseExtension)
+        public
+        onlyOwner
+    {
+        baseExtension = _newBaseExtension;
+    }
+
+    /* View / Pure functions */
+}
