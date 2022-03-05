1ae9fe116561a51355df01787545722212a5783e
Carlos Calderon <cdcalderon@gmail.com>
2022-03-04 22:29:26 -0600

    add deployment script

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,11 @@
+const { network } = require("hardhat");
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+  // If we are on a local development network, we need to deploy mocks!
+  if (chainId == 31337) {
+    log("Local network detected! Deploying mocks...");
+  }
+};
+module.exports.tags = ["all", "mocks"];
